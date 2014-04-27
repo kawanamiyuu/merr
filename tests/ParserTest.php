2e7638604356ab0b555de55821002320b03f836b
kawanamiyuu <i.am.really.you@gmail.com>
2014-04-27 21:20:58 +0900

    \Zend\Mail\Message の挙動を検証

--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -4,12 +4,24 @@ use Merr\Parser;
 
 class ParserTest extends PHPUnit_Framework_TestCase {
 
-    /**
-     * @test
-     */
-    public function インスタンス()
-    {
-        $this->assertInstanceOf("Merr\Parser", new Parser());
-    }
+	/**
+	 * @test
+	 */
+	public function インスタンス()
+	{
+		$this->assertInstanceOf("Merr\\Parser", new Parser());
+	}
+
+	/**
+	 * @test
+	 */
+	public function zend_mail_messageでパース()
+	{
+		$mail = getTestMail("01.plain_text_ascii.eml");
+		$message = \Zend\Mail\Message::fromString($mail);
+
+		$this->assertInstanceOf("\\Zend\\Mail\\Headers", $message->getHeaders());
+		$this->assertTrue(is_string($message->getBody()));
+	}
 }
  
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,14 @@
 <?php
 
 require_once __DIR__ . "/../vendor/autoload.php";
+
+define("TEST_DATA_DIR", __DIR__ . "/data");
+
+/**
+ * @param string $fileName テストメールファイル名
+ * @return string テストメール
+ */
+function getTestMail($fileName)
+{
+	return file_get_contents(TEST_DATA_DIR . "/" . $fileName);
+}
